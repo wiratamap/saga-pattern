133c94a79a4ee68278ff4fca6bc274c83a03ff99
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2020-02-01 21:05:34 +0700

    Add AuditModel boilerplate to account and aggregator service

--- service-aggregator/src/main/java/com/personal/sagapattern/ServiceAggregatorApplication.java
+++ service-aggregator/src/main/java/com/personal/sagapattern/ServiceAggregatorApplication.java
@@ -2,8 +2,12 @@ package com.personal.sagapattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@Configuration
+@EnableJpaAuditing
 public class ServiceAggregatorApplication {
 
 	public static void main(String[] args) {
