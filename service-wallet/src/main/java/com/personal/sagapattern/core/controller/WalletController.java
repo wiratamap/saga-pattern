68091dee779dd1312e9b9e6f4dc88e6444e8c77f
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2020-10-24 13:30:12 +0700

    add test for wallet controller

--- service-wallet/src/main/java/com/personal/sagapattern/core/controller/WalletController.java
+++ service-wallet/src/main/java/com/personal/sagapattern/core/controller/WalletController.java
@@ -10,19 +10,17 @@ import com.personal.sagapattern.core.service.WalletService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("wallet")
 @AllArgsConstructor
 public class WalletController {
 
     private final WalletService walletService;
 
-    @PostMapping("top-up")
+    @PostMapping("/transactions")
     public ResponseEntity<TopUpResponse> topUp(@Valid @RequestBody TopUpRequest topUpRequest)
             throws JsonProcessingException {
         TopUpResponse topUpResponse = walletService.topUp(topUpRequest);
---
+++ service-wallet/src/test/java/com/personal/sagapattern/core/controller/WalletControllerTest.java
@@ -0,0 +1,58 @@
+package com.personal.sagapattern.core.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.sagapattern.core.model.EventTopUp;
+import com.personal.sagapattern.core.model.dto.TopUpRequest;
+import com.personal.sagapattern.core.model.dto.TopUpResponse;
+import com.personal.sagapattern.core.repository.EventTopUpRepository;
+import com.personal.sagapattern.orchestration.service.SagaOrchestrationService;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@MockBean(SagaOrchestrationService.class)
+class WalletControllerTest {
+    @Autowired
+    private MockMvc client;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private EventTopUpRepository eventTopUpRepository;
+
+    @AfterEach
+    void tearDown() {
+        eventTopUpRepository.deleteAll();
+    }
+
+    @Test
+    void topUp_shouldReturnStatusOkAndTopUpResponse_whenPostTransactionsIsInvoked() throws Exception {
+        TopUpRequest topUpRequest = TopUpRequest.builder().cif("000000001").amount(10000).wallet("GO-PAY")
+                .destinationOfFund("00000000").build();
+        String topUpRequestJson = objectMapper.writeValueAsString(topUpRequest);
+        RequestBuilder request = MockMvcRequestBuilders.post("/transactions").content(topUpRequestJson)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = client.perform(request).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        TopUpResponse topUpResponse = objectMapper.readValue(result.getResponse().getContentAsString(),
+                TopUpResponse.class);
+
+        Assertions.assertEquals(topUpRequest.getCif(), topUpResponse.getCif());
+        Assertions.assertEquals(topUpRequest.getAmount(), topUpResponse.getAmount());
+        Assertions.assertEquals(topUpRequest.getWallet(), topUpResponse.getWallet());
+    }
+}
