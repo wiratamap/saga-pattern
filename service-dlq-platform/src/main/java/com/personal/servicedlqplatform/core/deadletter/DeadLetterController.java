601af041182d124260b11c7dff14720b49340967
Wiratama Paramasatya <wiratamaparamasatya@gmail.com>
2020-10-25 14:40:38 +0700

    able to delete dead letter and take action on dead letter queue

--- service-dlq-platform/src/main/java/com/personal/servicedlqplatform/core/deadletter/DeadLetterController.java
+++ service-dlq-platform/src/main/java/com/personal/servicedlqplatform/core/deadletter/DeadLetterController.java
@@ -1,13 +1,18 @@
 package com.personal.servicedlqplatform.core.deadletter;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import com.personal.servicedlqplatform.core.deadletter.dto.DeadLetterDeleteRequestDto;
 import com.personal.servicedlqplatform.core.deadletter.dto.DeadLetterResponseDto;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,9 +26,13 @@ public class DeadLetterController {
     public ResponseEntity<List<DeadLetterResponseDto>> fetchAll() {
         List<DeadLetter> availableDeadLetters = this.deadLetterService.fetchAll();
         List<DeadLetterResponseDto> deadLetters = availableDeadLetters.stream()
-                .map(DeadLetterResponseDto::convertFromEntity)
-                .collect(Collectors.toList());
+                .map(DeadLetterResponseDto::convertFromEntity).collect(Collectors.toList());
 
         return new ResponseEntity<>(deadLetters, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/dead-letters/{id}")
+    public void delete(@PathVariable UUID id, @RequestBody DeadLetterDeleteRequestDto deadLetterDeleteRequestDto) {
+        this.deadLetterService.delete(id, deadLetterDeleteRequestDto);
     }
 }
--- service-dlq-platform/src/test/java/com/personal/servicedlqplatform/core/deadletter/DeadLetterControllerTest.java
+++ service-dlq-platform/src/test/java/com/personal/servicedlqplatform/core/deadletter/DeadLetterControllerTest.java
@@ -3,15 +3,21 @@ package com.personal.servicedlqplatform.core.deadletter;
 import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.personal.servicedlqplatform.consumer.DeadLetterListener;
+import com.personal.servicedlqplatform.core.deadletter.dto.DeadLetterActionRequestDto;
+import com.personal.servicedlqplatform.core.deadletter.dto.DeadLetterDeleteRequestDto;
+import com.personal.servicedlqplatform.orchestration.service.SagaOrchestrationService;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -20,13 +26,16 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@MockBean(DeadLetterListener.class)
+@MockBean({ DeadLetterListener.class, SagaOrchestrationService.class })
 class DeadLetterControllerTest {
     @Autowired
     private MockMvc client;
 
     @Autowired
     private DeadLetterRepository deadLetterRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @AfterEach
     void tearDown() {
@@ -52,5 +61,20 @@ class DeadLetterControllerTest {
                         Matchers.equalTo(expectedOriginalMessage)))
                 .andExpect((ResultMatcher) MockMvcResultMatchers.jsonPath("$[0].originTopics[0].name",
                         Matchers.equalTo(expectedOriginTopic)));
+    }
+
+    @Test
+    void delete_shouldReturnStatusOk_whenDeleteDeadLettersIsInvoked() throws Exception {
+        DeadLetter availableDeadLetter = this.deadLetterRepository.save(this.deadLetter());
+        DeadLetterDeleteRequestDto deleteRequest = DeadLetterDeleteRequestDto.builder()
+                .deleteAction(DeadLetterActionRequestDto.SEND_TO_ORIGIN_TOPIC).build();
+        String deleteRequestJson = this.objectMapper.writeValueAsString(deleteRequest);
+        RequestBuilder request = MockMvcRequestBuilders.delete("/dead-letters/{id}", availableDeadLetter.getId())
+                .content(deleteRequestJson).contentType(MediaType.APPLICATION_JSON);
+
+        this.client.perform(request).andExpect(MockMvcResultMatchers.status().isOk());
+        DeadLetter deletedDeadletter = this.deadLetterRepository.findById(availableDeadLetter.getId()).orElse(null);
+
+        Assertions.assertNull(deletedDeadletter);
     }
 }
